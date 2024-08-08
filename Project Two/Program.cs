efbb393a98abe9d4ec5bb3c1db3e1a489ef79955
mf0zz13 <michael.foster31@outlook.com>
2024-08-08 14:25:06 -0400

    Added TrimZeros Method

--- Project Two/Program.cs
+++ Project Two/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Week5GroupProject
+{
+    class Project_2
+    {
+        static string TrimZeros(string num)
+        {
+            return num.Trim('0');
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TrimZeros("51230100"));
+            Console.ReadKey();
+        }
+    }
+}

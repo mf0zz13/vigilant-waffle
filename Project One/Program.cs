5aefa228ecd57fb87306d67afab0e516ef3fb224
Jacob Wright <wright.wjt@outlook.com>
2024-08-08 11:30:43 -0400

    Added CanBuyTwoChocs Method

--- Project One/Program.cs
+++ Project One/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Week5GroupProject
+{
+    class Project_1
+    {
+        static string CanBuyTwoChocs(int[] array, int money)
+        {
+            int[] sortedArray = Array.Sort[array, 0, array.Length];
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+

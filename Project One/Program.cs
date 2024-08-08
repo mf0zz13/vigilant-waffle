7df684e6ad9560dc36f5ca7e57715fa53db65071
Jacob Wright <wright.wjt@outlook.com>
2024-08-08 11:42:57 -0400

    added logic to CanBuyTwChocs method

--- Project One/Program.cs
+++ Project One/Program.cs
@@ -5,9 +5,26 @@ namespace Week5GroupProject
 {
     class Project_1
     {
-        static string CanBuyTwoChocs(int[] array, int money)
+        static string CanBuyTwoChocs(int[] sortedArray, int money)
         {
-            int[] sortedArray = Array.Sort[array, 0, array.Length];
+            Array.Sort(sortedArray);
+            int lowestPrice = sortedArray[0];
+
+            for (int i = 1; i < sortedArray.Length; i++)
+            {
+                int priceOfTwo = lowestPrice + sortedArray[i];
+                switch (priceofTwo <= money)
+                {
+                    case true:
+                        return $"You have {money - priceOfTwo:C2} left over ";
+                        break;
+                    default:
+                        return "You cannot afford two chocolates. ";
+                        break;
+
+                }
+
+            }
 
         }
 

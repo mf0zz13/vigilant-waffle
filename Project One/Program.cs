b852b1a12b9445d16040505ecd71c46372bcb910
mf0zz13 <115477550+mf0zz13@users.noreply.github.com>
2024-08-08 12:09:36 -0400

    Update Program.cs

--- Project One/Program.cs
+++ Project One/Program.cs
@@ -8,22 +8,17 @@ namespace Week5GroupProject
         static string CanBuyTwoChocs(int[] sortedArray, int money)
         {
             Array.Sort(sortedArray);
-            int lowestPrice = sortedArray[0];
-
-            for (int i = 1; i < sortedArray.Length; i++)
+            int priceOfTwo = sortedArray[0] + sortedArray[1];
+            switch (priceOfTwo <= money)
             {
-                int priceOfTwo = lowestPrice + sortedArray[i];
-                switch (priceOfTwo <= money)
-                {
-                    case true:
-                        return $"You have {money - priceOfTwo:C2} left over ";
-                        break;
-                    default:
-                        return "You cannot afford two chocolates. ";
-                        break;
+                case true:
+                    return $"You have {money - priceOfTwo:C2} left over ";
+                    break;
+                default:
+                    return "You cannot afford two chocolates. ";
+                    break;
 
 
-                }
             }
             return "Calcualtion Failed!";
         }

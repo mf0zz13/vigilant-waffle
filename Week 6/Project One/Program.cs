40598af7f26b75e250d7d905e96a17f71395dddc
mf0zz13 <michael.foster31@outlook.com>
2024-08-15 13:29:58 -0400

    Commented solution file

--- Week 6/Project One/Program.cs
+++ Week 6/Project One/Program.cs
@@ -4,18 +4,22 @@ namespace WeekSixGroupProject
 {
     class ProjectOne
     {
+        // Finds the most lucrative houses to rob that are not adjecent to other houses that will be robbed
         static int RobAHouse(int[] house)
         {
+            // creating a touple that will keep track of inital index and values
             (int index, int value)[] valuesIndex = new (int index, int value)[house.Length];
             int[] indexUsed = new int[house.Length];
             int sum = 0;
 
+            // For loop that takes inital array passed and puts values into new tuple array
             for (int i = 0; i < house.Length; i++)
             {
                 valuesIndex[i].index = i;
                 valuesIndex[i].value = house[i];
             }
 
+            // Nested for loop to conduct a Select swap in decending order by value
             for (int i = 0; i < house.Length - 1; i ++)
             {
                 int maxValPlace = i;
@@ -32,6 +36,8 @@ namespace WeekSixGroupProject
             sum += valuesIndex[0].value;
             indexUsed[0] = valuesIndex[0].index;
             int postion = 1;
+
+            // For loop to pick the house to rob based off if house is adjcent to a house that has already been robbed.
             for (int i = 1; i < house.Length; i++)
             {
                 int lessThan = (valuesIndex[i].index) - 1;
